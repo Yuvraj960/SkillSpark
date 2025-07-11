2b874b6180119b0f95b11a76ae2aedafb8246ebc
Yuvraj Gupta <yuvrajgupta956@gmail.com>
2025-07-11 09:01:19 +0530

    Removed faulty code

--- Code/frontend/src/api/axiosConfig.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"; 
- 
- 
-axios.defaults.baseURL = "http://localhost:5000"; 
- 
-// Optionally, set other defaults like timeout 
-axios.defaults.timeout = 10000; // 10 seconds 
- 
-export default axios;
